748001aa539da7bb0939c83bd7b923d638e4bf63
himanchal08 <khattrihimanchal17@gmail.com>
2026-08-16 23:10:19 +0530

    feat: add location permission handling to LiveMap component

--- components/LiveMap.tsx
+++ components/LiveMap.tsx
@@ -1,5 +1,7 @@
 import { StyleSheet, View } from 'react-native';
+import { useState, useEffect } from 'react';
 import * as MapLibreGL from '@maplibre/maplibre-react-native';
+import * as Location from 'expo-location';
 
 MapLibreGL.setAccessToken(null);
 
@@ -9,6 +11,23 @@ interface Props {
 }
 
 export function LiveMap({ routePoints, lineColor = '#fc4c02' }: Props) {
+  const [hasPermission, setHasPermission] = useState(false);
+
+  useEffect(() => {
+    let mounted = true;
+    const checkPerm = async () => {
+      const { status } = await Location.getForegroundPermissionsAsync();
+      if (mounted) setHasPermission(status === 'granted');
+    };
+    checkPerm();
+    
+    // Poll for permission changes since they might grant it by pressing 'Start'
+    const interval = setInterval(checkPerm, 2000);
+    return () => {
+      mounted = false;
+      clearInterval(interval);
+    };
+  }, []);
   const routeLine = {
     type: 'FeatureCollection',
     features: [
@@ -31,16 +50,19 @@ export function LiveMap({ routePoints, lineColor = '#fc4c02' }: Props) {
         attributionEnabled={false}
         mapStyle="https://basemaps.cartocdn.com/gl/dark-matter-gl-style/style.json"
       >
-        <MapLibreGL.Camera
-          followUserLocation={true}
-          followZoomLevel={16}
-          followUserMode={MapLibreGL.UserTrackingMode.FollowWithCourse}
-        />
-        
-        <MapLibreGL.UserLocation 
-          visible={true} 
-          showsUserHeadingIndicator={true}
-        />
+        {hasPermission && (
+          <>
+            <MapLibreGL.Camera
+              followUserLocation={true}
+              followZoomLevel={16}
+              followUserMode={MapLibreGL.UserTrackingMode.FollowWithCourse}
+            />
+            <MapLibreGL.UserLocation 
+              visible={true} 
+              showsUserHeadingIndicator={true}
+            />
+          </>
+        )}
 
         {routePoints.length > 1 && (
           <MapLibreGL.ShapeSource id="routeSource" shape={routeLine as any}>
